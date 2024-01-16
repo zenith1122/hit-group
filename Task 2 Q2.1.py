589f537749fa39a7d89f00011cd78f8476a03aa4
MdAbdullahalmamun371484 <153176432+MdAbdullahalmamun371484@users.noreply.github.com>
2024-01-17 00:36:20 +1100

    Assignment 2
    Co-Authored-By: zenith1122 <150229365+zenith1122@users.noreply.github.com>
    Co-Authored-By: Shem123-Rd <128105811+Shem123-Rd@users.noreply.github.com>
    Co-Authored-By: MdKhasru <156491211+MdKhasru@users.noreply.github.com>

--- Task 2 Q2.1.py
+++ Task 2 Q2.1.py
@@ -1,15 +1,3 @@
-
-
-import time
-current_time = int(time.time())
-generated_number = (current_time % 100) + 50 
-if generated_number % 2 == 0:
-    generated_number += 10
-    
-print(generated_number)
-
-pip install Image
-
 from PIL import Image
 
 # Load the original image
