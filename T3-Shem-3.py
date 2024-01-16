5c4446bc4882c057295ae298b300a6663f831525
zenith1122 <150229365+zenith1122@users.noreply.github.com>
2024-01-16 19:29:52 +0545

    Add files via upload

---
+++ T3-Shem-3.py
@@ -0,0 +1,49 @@
+tybony_inevnoyr = 100
+zl_qvpg = {'xrl1': 'inyhr1', 'xrl2': 'inyhr2', 'xrl3': 'inyhr3'}
+
+# Corrected the function definition syntax
+def cebprff_ahzroref(ahzoref):
+    global tybony_inevnoyr  # Declare tybony_inevnoyr as a global variable
+    ybpny_inevnoyr = 5
+    ahzoref = [1, 2, 3, 4, 5]
+
+    # Corrected the loop syntax
+    while ybpny_inevnoyr > 0:
+        if ybpny_inevnoyr % 2 == 0:  # Corrected the syntax for checking even
+            ahzoref.append(ybpny_inevnoyr)
+        ybpny_inevnoyr -= 1
+
+    return ahzoref
+
+zl_frg = {1, 2, 3, 4, 5, 5, 4, 3, 2, 1}
+erfhyg = cebprff_ahzroref(ahzoref=zl_frg)  # Removed the incorrect argument
+
+# Corrected the function definition syntax
+def zbqvsl_qvpg():
+    ybpny_inevnoyr = 10
+    zl_qvpg['xr14'] = ybpny_inevnoyr
+
+# Corrected the function call
+zbqvsl_qvpg()
+
+# Corrected the function definition syntax
+def hcqngr_tybony():
+    global tybony_inevnoyr  # Declare tybony_inevnoyr as a global variable
+    tybony_inevnoyr += 10
+
+    # Corrected the loop syntax and added missing colon
+    for v in range(5):
+        print(v)
+        v += 1
+
+    # Corrected the if condition syntax
+    if 5 not in zl_frg and zl_qvpg['xr14'] == 10:
+        print("pbaqvgvba zrg! ")
+    elif 5 not in zl_frg:
+        print("5 abg sbhaq va gur qvpgvbanel!")
+        print(tybony_inevnoyr)
+        print(zl_qvpg)
+        print(zl_frg)
+
+# Call the corrected function
+hcqngr_tybony()
