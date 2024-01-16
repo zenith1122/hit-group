14126239f653d699f1ae69b7a9391452bd32e7fd
Shem667 <156269998+Shem667@users.noreply.github.com>
2024-01-16 23:50:01 +0930

    Update T3-Shem-3.py
    some changes is done

--- T3-Shem-3.py
+++ T3-Shem-3.py
@@ -1,42 +1,42 @@
 tybony_inevnoyr = 100
 zl_qvpg = {'xrl1': 'inyhr1', 'xrl2': 'inyhr2', 'xrl3': 'inyhr3'}
 
-# Corrected the function definition syntax
+# the function definition syntax is corrected
 def cebprff_ahzroref(ahzoref):
-    global tybony_inevnoyr  # Declare tybony_inevnoyr as a global variable
+    global tybony_inevnoyr  # tybony_inevnoyr is decleared as a global variable
     ybpny_inevnoyr = 5
     ahzoref = [1, 2, 3, 4, 5]
 
     # Corrected the loop syntax
     while ybpny_inevnoyr > 0:
-        if ybpny_inevnoyr % 2 == 0:  # Corrected the syntax for checking even
+        if ybpny_inevnoyr % 2 == 0:  # the syntax for checking even is corrected
             ahzoref.append(ybpny_inevnoyr)
         ybpny_inevnoyr -= 1
 
     return ahzoref
 
 zl_frg = {1, 2, 3, 4, 5, 5, 4, 3, 2, 1}
-erfhyg = cebprff_ahzroref(ahzoref=zl_frg)  # Removed the incorrect argument
+erfhyg = cebprff_ahzroref(ahzoref=zl_frg)  # the incorrect argument is removed
 
-# Corrected the function definition syntax
+# the function definition syntax is corrected
 def zbqvsl_qvpg():
     ybpny_inevnoyr = 10
     zl_qvpg['xr14'] = ybpny_inevnoyr
 
-# Corrected the function call
+# the function call is corrected
 zbqvsl_qvpg()
 
-# Corrected the function definition syntax
+# the function definition syntax is corrected
 def hcqngr_tybony():
     global tybony_inevnoyr  # Declare tybony_inevnoyr as a global variable
     tybony_inevnoyr += 10
 
-    # Corrected the loop syntax and added missing colon
+    # the loop syntax is corrected and missing colon is added
     for v in range(5):
         print(v)
         v += 1
 
-    # Corrected the if condition syntax
+    # the if condition syntax is corrected
     if 5 not in zl_frg and zl_qvpg['xr14'] == 10:
         print("pbaqvgvba zrg! ")
     elif 5 not in zl_frg:
